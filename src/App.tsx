66fec01fdcbc7285bc2c32bb79209656184ebf9a
Spraymay <MaximPolyakovv@yandex.ru>
2023-01-25 14:25:35 +0300

    add mind map

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 import Glossary from './components/Glossary/Glossary'
 import Header from './components/Header/Header'
+import MindMap from './components/MindMap/MindMap'
+import { ICard } from './Interfaces/ICard'
 import { data } from './source/Source'
+
+import { Box } from '@mui/system'
+import { Tab } from '@mui/material'
+import { TabPanel, TabContext, TabList } from '@mui/lab';
+import { useState } from 'react'
+import Tabs from '@mui/material/Tabs'
+
+const TabStyle = {
+  color: 'white',
+
+}
 
 
 function App() {
+  const data_indexed: ICard[] = data.map((item, index) => { item.id = index + 1; return item })
+
+  const [currentTab, setCurrentTab] = useState('1')
+
+  const TabsHandleChange = (event: any, value: any) => {
+    setCurrentTab(value)
+  }
 
   return (
     <div className="App">
       <Header />
-      <Glossary source={data} />
+      <div className='container'>
+        <TabContext value={currentTab}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs
+              color='white'
+              onChange={TabsHandleChange}
+              sx={{
+                '& button': { width: '150px', fontSize: '20px' },
+                '& button:hover': { backgroundColor: '#FFFFFF', color: '#000000' },
+                '& button:focus': { borderBottom: '2px solid #FFFFFF', color: '#b8b8b8' },
+              }}
+            >
+              <Tab label="Глоссарий" value="1" color='white' sx={TabStyle} />
+              <Tab label="Mind-map" value="2" color='white' sx={TabStyle} />
+            </Tabs>
+          </Box>
+          <TabPanel value="1">
+            <Glossary source={data_indexed} />
+          </TabPanel>
+          <TabPanel value="2">
+            <MindMap />
+          </TabPanel>
+        </TabContext>
+      </div>
     </div>
   )
 }
---
+++ src/components/MindMap/MindMap.tsx
@@ -0,0 +1,20 @@
+
+import React from "react"
+
+
+export default function MindMap() {
+  return (
+    <div>
+      <iframe
+        width="100%"
+        height="600"
+        // frameBorder="0" 
+        src="https://www.mindmeister.com/maps/public_map_shell/2573665509/?width=600&height=400&z=auto&no_share=1&no_logo=1"
+      // scrolling="no" 
+      >
+        Ваш браузер не отображает фреймы. Пожалуйста, посетите
+        <a href="https://www.mindmeister.com/2573665509/" target="_blank">АИС</a> в MindMeister.
+      </iframe>
+    </div>
+  )
+}
